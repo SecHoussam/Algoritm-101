ed96ce1610ec367b4dfca05d5fbdce29546143f4
Houssam Miliani <149241633+SecHoussam@users.noreply.github.com>
2026-02-02 17:01:22 +0100

    Update p1.cpp

--- Level-1/p1.cpp
+++ Level-1/p1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+// build Your Simple Own Power function :
+
+int ReadNumber(){
+    int number;
+
+    cout << " Enter the Number : ";
+    cin >> number;
+    return number;
+
+}
+
+void PowerOf2_3_4(int number){
+
+    int a , b , c;
+
+    a = number * number;
+    b = number * number * number;
+    c = number * number * number * number;
+
+    cout << "- Numner ^2 =  "<< a << endl;
+    cout << "- Numner ^3 =  "<< b << endl;
+    cout << "- Numner ^4 =  "<< c << endl;
+}
+
+int main(){
+
+    PowerOf2_3_4(ReadNumber());
+}
